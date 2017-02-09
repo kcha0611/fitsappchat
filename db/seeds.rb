62e8e2ebd9cb436f6b552b8598263cae88f3f4da
Ken_Cha <kencha0611@gmail.com>
2017-02-08 23:07:06 -0800

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(first_name: "Ken", last_name: "Cha", email: "a@a.com", password: "kencha", role: "Admin")
+User.create(first_name: "Gina", last_name: "Kim", email: "b@b.com", password: "kencha", role: "Member")
+User.create(first_name: "Tiff", last_name: "Wang", email: "c@c.com", password: "kencha", role: "Trainer")
+Chat.create(subject: "For Members", body: "How are you all doing today?", recipient: "Member")
+Chat.create(subject: "Hello", body: "Why do Admins get to delete our messages?", recipient: "Member")
+Chat.create(subject: "For Trainers", body: "How certified are you guys?", recipient: "Trainer")
+Chat.create(subject: "For Trainers", body: "How often do I need to workout?", recipient: "Trainer")
+Chat.create(subject: "For Admins", body: "This site is great keep up the good work!", recipient: "Admin")
+Chat.create(subject: "For Admins", body: "Can anyone be an Admin?", recipient: "Admin")
